06a04b2671b241471e232b6effc748866fdb6264
Mikhail Voloshin (desktop workstation) <mvol@live.com>
2024-01-09 20:47:40 -0500

    Unit tests complete for tooth ratio calculator.

--- jsprototype/planetgearfight/test/toothratios.test.ts
+++ jsprototype/planetgearfight/test/toothratios.test.ts
@@ -16,3 +16,39 @@ test('a valid tooth ratio with fractional counts is invalid', () => {
   const r = GearAssembly.checkValidToothCounts(6, 4.5, 15);
   expect(r).toBe(false);
 });
+
+test('underspecification is invalid', () => {
+  let r = GearAssembly.checkValidToothCounts(null, null, 30);
+  expect(r).toBe(false);
+
+  r = GearAssembly.checkValidToothCounts(12, null, null);
+  expect(r).toBe(false);
+
+  r = GearAssembly.checkValidToothCounts(null, 9, null);
+  expect(r).toBe(false);
+});
+
+test('solve for Sun', () => {
+  const r = GearAssembly.checkValidToothCounts(null, 9, 30);
+  expect(r).toBe(12);
+});
+
+test('solve for Planet', () => {
+  const r = GearAssembly.checkValidToothCounts(12, null, 30);
+  expect(r).toBe(9);
+});
+
+test('solve for Ring', () => {
+  const r = GearAssembly.checkValidToothCounts(12, 9, null);
+  expect(r).toBe(30);
+});
+
+test('non-integer solution is invalid', () => {
+  const r = GearAssembly.checkValidToothCounts(6, null, 15);
+  expect(r).toBe(false);
+});
+
+test('zero does not mean null', () => {
+  const r = GearAssembly.checkValidToothCounts(12, 0, 30);
+  expect(r).toBe(false);
+});
